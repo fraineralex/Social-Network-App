0c266d8e44ec904a12d6fa17654b1fdc3802378d
Alexander Echavarría <95458196+Alexander2902@users.noreply.github.com>
2022-08-23 19:03:21 -0400

    Token Fixed up

--- app.js
+++ app.js
@@ -97,6 +97,7 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.newSession;
   res.locals.errorMessages = errors;
   res.locals.hasErrorMessages = errors.length > 0;
+  res.locals.csrfToken = req.csrfToken();
   
   next();
 });
