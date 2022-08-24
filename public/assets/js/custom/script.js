8db38f4380f6198750660dccfa32c95688d7ac30
Frainer Alexander <fraineralex2001@gmail.com>
2022-08-24 01:24:21 -0400

    add delete confirm message to events

--- public/assets/js/custom/script.js
+++ public/assets/js/custom/script.js
@@ -21,33 +21,59 @@ modeSwitch.addEventListener("click", () => {
 
 function ShowAlert(postId, csrfToken) {
   Swal.fire({
-      title: '¿Seguro que quieres eliminar esta publicación?',
-      text: "Una vez eliminada no podrás recuperla.",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#d33',
-      cancelButtonColor: '#3085d6',
-      confirmButtonText: 'Eliminar',
-      reverseButtons: true
+    title: "¿Seguro que quieres eliminar esta publicación?",
+    text: "Una vez eliminada no podrás recuperla.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Eliminar",
+    reverseButtons: true,
   }).then((result) => {
-      if (result.isConfirmed) {
-          Swal.fire(
-              '¡Eliminada!',
-              'Tu publicación ha sido eliminada satisfactoriamente.',
-              'success'
-          )
+    if (result.isConfirmed) {
+      Swal.fire(
+        "¡Eliminada!",
+        "Tu publicación ha sido eliminada satisfactoriamente.",
+        "success"
+      );
 
-          setTimeout(() => {
-              let form = document.createElement('form');
-              form.action = '/delete-post';
-              form.method = 'POST';
-              form.innerHTML = `<input type="hidden" value="${postId}" name="PostId">`;
-              form.innerHTML = `<input type="hidden" name="_csrf" value="${csrfToken}">`;
-              document.body.append(form);
-              form.submit();
-          }, 2000);
-      }
-  })
+      setTimeout(() => {
+        let form = document.createElement("form");
+        form.action = "/delete-post";
+        form.method = "POST";
+        form.innerHTML = `<input type="hidden" value="${postId}" name="PostId"> <input type="hidden" name="_csrf" value="${csrfToken}">`;
+        document.body.append(form);
+        form.submit();
+      }, 2000);
+    }
+  });
 }
 
+function DeleteConfirm(id, item, title, successMessage) {
+  Swal.fire({
+    title: `¿${title}?`,
+    text: "Una vez se ha eliminado no se podrá recuperar.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "Eliminar",
+    reverseButtons: true,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire(
+        "Listo!",
+        `${successMessage}.`,
+        "success"
+      );
 
+      setTimeout(() => {
+        let form = document.createElement("form");
+        form.action = `/delete-${item}/${id}`;
+        form.method = "GET";
+        document.body.append(form);
+        form.submit();
+      }, 2000);
+    }
+  });
+}
